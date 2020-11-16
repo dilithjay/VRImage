f1e595cc85d38fd8fd7f86ee9006094417723f69
Dilith Jayakody <54039395+dilithjay@users.noreply.github.com>
2020-11-16 23:58:51 +0530

    Update warp.py

--- warp.py
+++ warp.py
@@ -15,7 +15,8 @@ def mousePoints(event, x, y, flags, params):
         print(counter)
 
 
-for i in range(595, 600):
+# set range based on increment
+for i in range(600):
     img = cv2.imread(image_path + str(i) + '.png')
     while True:
         if counter == 4:
@@ -28,8 +29,6 @@ for i in range(595, 600):
             cv2.imshow("Output Image", out)
         if counter > 0:
             cv2.destroyWindow("Output Image")
-        """for x in range(4):
-            cv2.circle(img, (circles[x][0], circles[x][1]), 3, (0, 255, 0), cv2.FILLED)"""
 
         cv2.imshow("Original"+str(i), cv2.resize(img[540:1620, 540:1620], (720, 720)))
         cv2.setMouseCallback("Original"+str(i), mousePoints)
